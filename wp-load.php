b9c7d620756b2c00e5d237f35f2f2f3cae6ac84d
Ben Rutland <ben@10degrees.uk>
2023-01-25 12:20:42 +0000

    changed vendor require for cli

--- wp-load.php
+++ wp-load.php
@@ -1,14 +1,5 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-try {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST'])->notEmpty();
-} catch (Exception $e) {
-    die($e->getMessage());
-}
 /**
  * Bootstrap file for setting the ABSPATH constant
  * and loading the wp-config.php file. The wp-config.php
